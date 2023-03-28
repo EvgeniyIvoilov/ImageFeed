ca3fd4025bd8573a010b29d7c242b60366a3a354
EvgeniyIvoilov <ivoylovevgeniy@yandex.ru>
2023-03-28 20:30:58 +0300

    sprint_10

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "jE3A396k2dSADY3iiJnJ4B7Tarw3dxW3e_ifurR98CY"
+let SecretKey = "QtlwYaeNeTP7JPUP6DVL4oFE9W2Wyo6330sVkmMGp9I"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
